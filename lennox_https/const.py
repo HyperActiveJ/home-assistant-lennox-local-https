972fb03d13bb22792be8193a9d8b2e8deb06e410
HyperActiveJ <6298301+HyperActiveJ@users.noreply.github.com>
2021-10-23 08:31:14 -0700

    Update const.py

--- lennox_https/const.py
+++ lennox_https/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "lennox_https"
 DEFAULT_NAME = "LennoxHTTPS"
-DEFAULT_HOST = "furnace.jorgensen.xyz"
+DEFAULT_HOST = "furnace.xxx.xyz"
 ATTR_STATUS_DESCRIPTION = "status_description"
 ATTR_MANUFACTURER = "Lennox"
 
